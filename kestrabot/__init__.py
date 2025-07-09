a1267de5c7928b17f36ba7edc27dfaa83db3c6a1
xdatanomad <parham.parvizi@gmail.com>
2025-07-09 12:09:42 -0400

    fixing circular import issue

--- kestrabot/__init__.py
+++ kestrabot/__init__.py
@@ -8,6 +8,5 @@ __version__ = "0.1.0"
 __author__ = "Parham Parvizi"
 __email__ = "parham.parvizi@gmail.com"
 
-from .app import KestraBotApp, run_app
 
-__all__ = ["KestraBotApp", "run_app"]
+__all__ = ["__version__", "__author__", "__email__"]
